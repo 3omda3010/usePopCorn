62fdb2f5fb480445a6175c33ed07d28cfb3558a1
3omda <memad7628@gmail.com>
2025-11-09 09:13:48 +0200

    Change API URL from HTTP to HTTPS for secure movie details fetching

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ function MovieDetails({
       async function getMovieDetails() {
         setLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
 
         const data = await res.json();
